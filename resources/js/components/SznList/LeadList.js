6c5cd384ff9caadd8a4572eb2d407ebd29ec05ee
Arif Swozon <swazan.arif@gmail.com>
2020-04-14 11:31:58 +0600

    react production build

--- resources/js/components/SznList/LeadList.js
+++ resources/js/components/SznList/LeadList.js
@@ -47,45 +47,47 @@ function LeadList(props) {
     }, [state.currentPage, state.perPage, state.sortBy, state.sortType]);
 
     const skeletonLoader = () => {
-        return <ContentLoader 
-                    speed={2}
-                    viewBox="0 0 945 500"
-                    backgroundColor="#f3f3f3"
-                    foregroundColor="#dad8d8"
-                >
-                    <rect x="33" y="36" rx="0" ry="0" width="92" height="90" /> 
-                    <rect x="144" y="41" rx="0" ry="0" width="196" height="15" /> 
-                    <rect x="144" y="69" rx="0" ry="0" width="353" height="12" /> 
-                    <rect x="143" y="92" rx="0" ry="0" width="399" height="18" /> 
-                    <rect x="143" y="116" rx="0" ry="0" width="51" height="14" /> 
-                    <rect x="205" y="118" rx="0" ry="0" width="298" height="12" /> 
-                    <rect x="517" y="116" rx="0" ry="0" width="26" height="15" /> 
-                    <rect x="0" y="10" rx="0" ry="0" width="13" height="487" /> 
-                    <rect x="-29" y="2" rx="0" ry="0" width="1001" height="11" /> 
-                    <rect x="930" y="7" rx="0" ry="0" width="66" height="490" /> 
-                    <rect x="6" y="358" rx="0" ry="0" width="2" height="15" /> 
-                    <rect x="5" y="484" rx="0" ry="0" width="935" height="13" /> 
-                    <rect x="797" y="32" rx="0" ry="0" width="44" height="28" /> 
-                    <rect x="854" y="32" rx="0" ry="0" width="56" height="28" /> 
-                    <rect x="43" y="186" rx="0" ry="0" width="100" height="47" /> 
-                    <rect x="255" y="186" rx="0" ry="0" width="100" height="47" /> 
-                    <rect x="476" y="185" rx="0" ry="0" width="100" height="47" /> 
-                    <rect x="693" y="184" rx="0" ry="0" width="100" height="47" /> 
-                    <rect x="7" y="242" rx="0" ry="0" width="952" height="17" /> 
-                    <rect x="33" y="281" rx="0" ry="0" width="92" height="90" /> 
-                    <rect x="144" y="286" rx="0" ry="0" width="196" height="15" /> 
-                    <rect x="144" y="314" rx="0" ry="0" width="353" height="12" /> 
-                    <rect x="143" y="337" rx="0" ry="0" width="399" height="18" /> 
-                    <rect x="143" y="361" rx="0" ry="0" width="51" height="14" /> 
-                    <rect x="205" y="363" rx="0" ry="0" width="298" height="12" /> 
-                    <rect x="517" y="361" rx="0" ry="0" width="26" height="15" /> 
-                    <rect x="797" y="277" rx="0" ry="0" width="44" height="28" /> 
-                    <rect x="854" y="277" rx="0" ry="0" width="56" height="29" /> 
-                    <rect x="43" y="431" rx="0" ry="0" width="100" height="47" /> 
-                    <rect x="255" y="431" rx="0" ry="0" width="100" height="47" /> 
-                    <rect x="476" y="430" rx="0" ry="0" width="100" height="47" /> 
-                    <rect x="693" y="429" rx="0" ry="0" width="100" height="47" />
-                </ContentLoader>
+        return <div className="content-loader-wrapper">
+                    <ContentLoader 
+                        speed={2}
+                        viewBox="0 0 945 500"
+                        backgroundColor="#f3f3f3"
+                        foregroundColor="#dad8d8"
+                    >
+                        <rect x="33" y="36" rx="0" ry="0" width="92" height="90" /> 
+                        <rect x="144" y="41" rx="0" ry="0" width="196" height="15" /> 
+                        <rect x="144" y="69" rx="0" ry="0" width="353" height="12" /> 
+                        <rect x="143" y="92" rx="0" ry="0" width="399" height="18" /> 
+                        <rect x="143" y="116" rx="0" ry="0" width="51" height="14" /> 
+                        <rect x="205" y="118" rx="0" ry="0" width="298" height="12" /> 
+                        <rect x="517" y="116" rx="0" ry="0" width="26" height="15" /> 
+                        <rect x="0" y="10" rx="0" ry="0" width="13" height="487" /> 
+                        <rect x="-29" y="2" rx="0" ry="0" width="1001" height="11" /> 
+                        <rect x="930" y="7" rx="0" ry="0" width="66" height="490" /> 
+                        <rect x="6" y="358" rx="0" ry="0" width="2" height="15" /> 
+                        <rect x="5" y="484" rx="0" ry="0" width="935" height="13" /> 
+                        <rect x="797" y="32" rx="0" ry="0" width="44" height="28" /> 
+                        <rect x="854" y="32" rx="0" ry="0" width="56" height="28" /> 
+                        <rect x="43" y="186" rx="0" ry="0" width="100" height="47" /> 
+                        <rect x="255" y="186" rx="0" ry="0" width="100" height="47" /> 
+                        <rect x="476" y="185" rx="0" ry="0" width="100" height="47" /> 
+                        <rect x="693" y="184" rx="0" ry="0" width="100" height="47" /> 
+                        <rect x="7" y="242" rx="0" ry="0" width="952" height="17" /> 
+                        <rect x="33" y="281" rx="0" ry="0" width="92" height="90" /> 
+                        <rect x="144" y="286" rx="0" ry="0" width="196" height="15" /> 
+                        <rect x="144" y="314" rx="0" ry="0" width="353" height="12" /> 
+                        <rect x="143" y="337" rx="0" ry="0" width="399" height="18" /> 
+                        <rect x="143" y="361" rx="0" ry="0" width="51" height="14" /> 
+                        <rect x="205" y="363" rx="0" ry="0" width="298" height="12" /> 
+                        <rect x="517" y="361" rx="0" ry="0" width="26" height="15" /> 
+                        <rect x="797" y="277" rx="0" ry="0" width="44" height="28" /> 
+                        <rect x="854" y="277" rx="0" ry="0" width="56" height="29" /> 
+                        <rect x="43" y="431" rx="0" ry="0" width="100" height="47" /> 
+                        <rect x="255" y="431" rx="0" ry="0" width="100" height="47" /> 
+                        <rect x="476" y="430" rx="0" ry="0" width="100" height="47" /> 
+                        <rect x="693" y="429" rx="0" ry="0" width="100" height="47" />
+                    </ContentLoader>
+                </div>
     };
 
 
