2ba306ddc23ae76be2fe930a7e1b4ef1792b850f
Arif Swozon <swazan.arif@gmail.com>
2020-04-23 13:53:25 +0600

    https support

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Config;
 use Illuminate\Support\ServiceProvider;
+use Str;
+use URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (Str::contains(Config::get('app.url'), 'https://')) {
+            URL::forceScheme('https');
+        }
     }
 }
