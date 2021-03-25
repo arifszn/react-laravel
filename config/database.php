33451848a16a85730a7949b2529ad0b28471b9c2
Ariful Alam <arif@orangetoolz.com>
2021-03-25 12:25:33 +0600

    Add heroku_pgsql db connection

--- config/database.php
+++ config/database.php
@@ -90,6 +90,18 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
         ],
+        'heroku_pgsql' => [
+            'driver' => 'pgsql',
+            'host' => @parse_url(getenv("DATABASE_URL"))['host'],
+            'port' => env('DB_PORT', '5432'),
+            'database' => @substr(parse_url(getenv("DATABASE_URL"))['path'],1),
+            'username' => @parse_url(getenv("DATABASE_URL"))['user'],
+            'password' => @parse_url(getenv("DATABASE_URL"))['pass'],
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
+            'sslmode' => 'prefer',
+        ],
 
     ],
 
